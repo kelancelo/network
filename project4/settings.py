490cac2f21df04efd4ee0e0387ae95346b389d6c
Miguel Angelo L. Delos Reyes <33194805+kelancelo@users.noreply.github.com>
2022-11-03 11:02:38 +0800

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -148,6 +148,11 @@ STATIC_URL = '/static/'
 # Tell Django to copy statics to the `staticfiles` directory
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Turn on WhiteNoise storage backend that takes care of compressing static files
 # and creating unique names for each version so they can safely be cached forever.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
